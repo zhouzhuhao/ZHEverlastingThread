b47e9d206c681339c5553771dccfc046b6567de5
Johnzhuhaochow <johnzhuhaochow@gmail.com>
2019-05-23 08:58:22 +0800

    修改代码

--- ZHEverlastingThread/Classes/ZHEverlastingThread.m
+++ ZHEverlastingThread/Classes/ZHEverlastingThread.m
@@ -26,32 +26,6 @@
 {
 	if (self = [super init]) {
 		self.innerThread = [[ZHThread alloc] initWithTarget:self selector:@selector(zh_threadTask) object:nil];
-		
-//		[[ZHThread alloc] initWithBlock:^{
-//			NSLog(@"begin----");
-//
-//			// 创建上下文（要初始化一下结构体）
-//			CFRunLoopSourceContext context = {0};
-//
-//			// 创建source
-//			CFRunLoopSourceRef source = CFRunLoopSourceCreate(kCFAllocatorDefault, 0, &context);
-//
-//			// 往Runloop中添加source
-//			CFRunLoopAddSource(CFRunLoopGetCurrent(), source, kCFRunLoopDefaultMode);
-//
-//			// 销毁source
-//			CFRelease(source);
-//
-//			// 启动
-//			CFRunLoopRunInMode(kCFRunLoopDefaultMode, 1.0e10, false);
-//
-//			//            while (weakSelf && !weakSelf.isStopped) {
-//			//                // 第3个参数：returnAfterSourceHandled，设置为true，代表执行完source后就会退出当前loop
-//			//                CFRunLoopRunInMode(kCFRunLoopDefaultMode, 1.0e10, true);
-//			//            }
-//
-//			NSLog(@"end----");
-//		}];
 		[self.innerThread start];
 	}
 	return self;
